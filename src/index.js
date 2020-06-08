87f72dfc5723f249194dddad358327853964f1c2
grizzlyThresher <grizzlyThresher@gmail.com>
2020-06-07 23:08:39 -0400

    Moves Role data into separate JSON file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const config = require('./config/config.json');
+const commonRules = require('./serverSpecifics/common.json');
+//const customRules = require('./serverSpecifics/custom.json');
 const Discord = require('discord.js')
 
 
@@ -10,169 +12,31 @@ const client = new Discord.Client();
 
 const PREFIX = "^^";
 
-
-
-// Used to keep track of available roles
-const globallyTrackedRoles = ['Hunter','Fighter', 'Ranger', 'Gunner', 'Force', 'Techer', 'Braver', 'Bouncer', 'Summoner'];
-
-
-
 // When the bot is connected and ready, log to console.
 client.on('ready', () => {
    console.log('Connected and ready.');
    console.log("Connected as " + client.user.tag);
    let guilds = client.guilds.cache;
    let neededRoles = new Map();
-    globallyTrackedRoles.forEach(newRole => {
+    commonRules.commonly_tracked_roles.forEach(newRole => {
         neededRoles.set(newRole, {name:newRole, used:false});
     });
    guilds.forEach((guild) => {
    	let curRoles = neededRoles;
-//   	console.log(curRoles);
    	guild.roles.cache.forEach((role) => {
    		if (curRoles.has(role.name)) {
    			curRoles.set(role.name, {name:role.name, used:true});
    		}
    	});
-    // Checks through all the current roles. If it does not exist, it creates the role.
+
+    // Checks through all the current roles. If a role being tracked for this server does not exist, it creates the role.
    	curRoles.forEach((role) => {
    		if (!role.used) {
-   			console.log(role);
-        switch (role.name) {
-          case "Hunter":
-              console.log("Creating Hunter role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Hunter',
-                    color: '0x7f1510',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Hunter',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Fighter":
-              console.log("Creating Fighter role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Fighter',
-                    color: '0x9a2c75',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Fighter',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Ranger":
-              console.log("Creating Ranger role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Ranger',
-                    color: '0x1c3298',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Ranger',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Gunner":
-              console.log("Creating Gunner role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Gunner',
-                    color: '0x2183bd',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Gunner',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Force":
-              console.log("Creating Force role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Force',
-                    color: '0xb6b936',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Force',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Techer":
-              console.log("Creating Techer role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Techer',
-                    color: '0x8d6309',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Techer',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Bouncer":
-              console.log("Creating Bouncer role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Bouncer',
-                    color: '0x7aae09',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Bouncer',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Braver":
-              console.log("Creating Braver role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Braver',
-                    color: '0x008f29',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Braver',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "Summoner":
-              console.log("Creating Summoner role.");
-              guild.roles.create({
-                  data: {
-                    name: 'Summoner',
-                    color: '0xd92667',
-                    mentionable: false
-                  },
-                  reason: 'Keep track of who plays Summoner',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          case "test":
-              console.log("Creating test role.");
-              // Create a new role with data and a reason
-              guild.roles.create({
-                  data: {
-                    name: 'test',
-                    color: '0x0000ff',
-                    mentionable: true
-                  },
-                  reason: 'role to test role addition',
-              })
-                .then(console.log)
-                .catch(console.error);
-              break;
-          default:
-              console.log(`Will add ${role.name} to roles soon.`);
-              break;
+        if (commonRules.commonly_tracked_roles.includes(role.name)) {
+          console.log("Creating " + role.name + " role in Server: " + guild.id);
+          guild.roles.create(commonRules.role_info[role.name])
+           .then(console.log)
+            .catch(console.error);
         }
    		}
    	});
@@ -327,7 +191,7 @@ function addRoleCommand(arguments, message) {
           message.guild.members.cache.get(message.author.id)
           .roles.add(roleId)
            .then(console.log)
-           .catch(console.error);
+            .catch(console.error);
           message.channel.send("Okie dokie " + message.author.toString() +"! You now have the role of \"" + roleName + "\" :partying_face:");
         }
       }
@@ -362,7 +226,7 @@ function removeRoleCommand(arguments, message) {
       message.guild.members.cache.get(message.author.id)
           .roles.remove(roleId)
            .then(console.log)
-           .catch(console.error);
+            .catch(console.error);
       message.channel.send("Okie dokie " + message.author.toString() +"! You no longer have the Role of \"" + roleName + "\" :slight_smile:");
     } else {
       message.channel.send("Hey there " + message.author.toString() + ", it looks like you either don't currently have the Role of \"" +
